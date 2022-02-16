b230f29ff38cd951638119b3d17ec060d1ac3071
Daniil Lysenko <productivemouse@gmail.com>
2022-02-16 16:05:36 +0300

    update powershell profile

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,3 +1,9 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module Terminal-Icons
+Set-PoshPrompt -Theme material
+
 # PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
